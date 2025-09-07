cc5964648747d4ccae75ae7600e3c66e9a9f0747
IlakiyaSiva <133512460+IlakiyaSiva@users.noreply.github.com>
2025-09-07 22:35:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -595,4 +595,9 @@ def book_appointment():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(
+        host='0.0.0.0',
+        port=443,
+        debug=True,
+        ssl_context=('/etc/ssl/certs/selfsigned.crt', '/etc/ssl/private/selfsigned.key')
+    )
